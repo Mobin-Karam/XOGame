c93dabce2a0ad95bf20aaa3a6b8c9d2425fd4db2
Mobin-Karam <mohammadmobinkaram@gmail.com>
2024-01-30 22:11:13 +0330

    add features

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
-const box = document.querySelectorAll(".board__row__box");
+document.addEventListener("DOMContentLoaded", () => {
+  const box = document.querySelectorAll(".board__row__box");
+  const header = document.querySelector(".header");
 
-let initialValue = ["O", "X"];
-let turn = 0;
+  let initialValue = ["O", "X"];
+  const winnerStates = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+  ];
+  const gameState = [];
 
-if (turn) {
+  let turn = 0;
+  header.textContent = `Turn : O`;
+
+  // Click
+
   for (let i = 0; i < box.length; i++) {
     box[i].addEventListener("click", () => {
-      box[i].textContent = initialValue[turn];
-      turn = 1;
+      if (!turn) {
+        box[i].textContent = initialValue[turn];
+        header.textContent = `Turn : X`;
+        gameState.push("O");
+        console.log(gameState);
+        turn = 1;
+      } else {
+        box[i].textContent = initialValue[turn];
+        header.textContent = `Turn : O`;
+        gameState.push("X");
+        console.log(gameState);
+        turn = 0;
+      }
     });
   }
-} else {
-  for (let i = 0; i < box.length; i++) {
-    box[i].addEventListener("click", () => {
-      box[i].textContent = initialValue[turn];
-      turn = 0;
-    });
-  }
-}
+});
